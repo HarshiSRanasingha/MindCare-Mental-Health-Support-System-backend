5ebed75a11bd8688fb102c4fccd99919aa449cf1
Harshi S Ranasinghe <harshanisadunika99@gmail.com>
2026-05-10 11:20:11 +0530

    Implemented CRUD controllers

---
+++ controllers/requestController.js
@@ -0,0 +1,85 @@
+import Request from '../models/requestModel.js';
+
+// CREATE REQUEST
+export const createRequest = async (req, res) => {
+
+  try {
+
+    const request = await Request.create(req.body);
+
+    res.status(201).json(request);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+};
+
+// GET ALL REQUESTS
+export const getRequests = async (req, res) => {
+
+  try {
+
+    const requests = await Request.find()
+      .populate('userId')
+      .populate('counselorId');
+
+    res.json(requests);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+};
+
+// UPDATE REQUEST
+export const updateRequest = async (req, res) => {
+
+  try {
+
+    const updatedRequest = await Request.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updatedRequest);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+};
+
+// DELETE REQUEST
+export const deleteRequest = async (req, res) => {
+
+  try {
+
+    await Request.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: 'Request Deleted Successfully'
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+};
